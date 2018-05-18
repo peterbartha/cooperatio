70cdfdda14e73376079f1deeb6390bbac819279c
Peter Bartha <mail@peterbartha.com>
2018-05-18 11:46:41 +0200

    build(index): export the public API of the framework

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// Client bundle
+export * from './adapters/codeMirrorAdapter';
+export * from './adapters/webSocketAdapter';
+export * from './client/editorClient';
+export * from './client/editorAdapter';
+export * from './operations/patch';
+
+// Server bundle
+export * from './server/editorWebSocketServer';
